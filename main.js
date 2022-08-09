ea561c1f308e9dbbb576092b957e59b8e48b1c54
Thilanka Rodrigo <65930513+Thilanka0824@users.noreply.github.com>
2022-08-09 09:38:13 -0700

    ace logic partially done

--- main.js
+++ main.js
@@ -21,7 +21,7 @@
 -load all the card values into an array that i'll randomize. not sure what to do with the ace(1 or 11)
 
 -use the dealtCards arr to keep track each card that was dealt. 
-    -the number of cards removed from the deck will be subtracted from the total number of available cards to be dealt from
+    -will be used to keep track of html id's of each card that was dealt
 
 -user makes bet (starting value of $50)
     -save bet amount in playerBet variable
@@ -32,7 +32,7 @@
 -deal the starting cards from shuffledArray 
     -1 to the player, 1 to the dealer, 1 to the player, 1 to the dealer(whole card. will not be visible to player)
 -put dealt cards into respective players card array(holds cards to be added up for total score)
--remove dealt cards from shuffleArray (.pop)
+-remove dealt cards from shuffleArray (.shift)
 
 ##########################################
 
@@ -50,7 +50,7 @@
     
     -player && dealer
         if(playerTotalScore === 21 && dealerTotalScore === 21){
-            push
+        .unshift
             add 0 to playerWallet
         }
 
@@ -79,6 +79,7 @@
             add first number in shuffleArray to thePlayerCards
             show player their card
             check if(playerCards) has an 11 in it
+                -ace adjuster
 
         }
     
@@ -94,27 +95,43 @@
 
 
 -if the player chooses to stand AND player score is less than 21
-    
+    -then move on to the dealers turn
+
 -if dealer score is 17 or more, dealer stands.
         -if player score is more than dealer score. player wins
             -adds bet amount to player wallet
             -will need a function to handle all the resets 
+        
+        -if player score is less than the dealer, dealer wins
+            -player bet is subtracted from player wallet
+            -bet resets
 
+-if the dealer score is less than 17, dealer takes another card
+    -the dealer will continue to hit until their score is 17 or more
+        -ace logic will apply here
+    -if the dealer busts, then every player that is still in the game wins
+        -adds bet amount to payer wallet
+        -resets the everything for a new round.
 
-
-
-
-
-
-
-
-
+-will use two diff resets.
+    -one to reset the round.
+        -reset bet amount
+        -clear the cards for the round
+    -one to reset the game
+        -after a a player loses all their money OR a player presses the New game button
+            -reset bet amount
+            -reset player wallet to $500
+            -reset all the cards being shown
+            -re-shuffle the shuffledArray
+            -reset Scorekeeper variables
 
 
 *********************
 *** stretch goals *** 
+*********************
 
 -have multiple players
+-leverage an api to show motivational quotes when a button is pressed
 -6 deck shoe
 -loading page with an ability to create multiple users
 
@@ -123,69 +140,132 @@
 const playerCards = [] //holds the players cards //used by the score keeper
 const dealerCards = [] //holds the dealers cards //used by the score keeper
 
-const playerWallet = 500
+let playerWallet = 500
+
+
+let playerTotalScore = 0
+let playerFinalScore = 0
+let dealerTotalScore = 0
 
 const fullDeck = [2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11]
 
-const shuffledArray = fullDeck.sort((a, b) => 0.5 - Math.random()); //randomizes the deck //Math.random returns a random number between 0 and 1
+let shuffledArray = fullDeck.sort((a, b) => 0.5 - Math.random()); //randomizes the deck //Math.random returns a random number between 0 and 1
 const dealtCards = []
 const gameOver = false
 
 
 
 //QuerySelectors
-// hitButton = document.querySelector('#hit-button')
-// standButton = document.querySelector('#stand-button')
+hitButton = document.querySelector('#hit-button')
+standButton = document.querySelector('#stand-button')
 
 // while(gameOver === false){
 // for(let i = 0; i < shuffledArray; i++){
-//     thePlayer.push(shuffledArray[i])
+//     thePlayer.unshift(shuffledArray[i])
 //     console.log(`Player has: ${shuffledArray[i]}`) //show the player their card
-//     shuffledArray.pop([i]) // remove the card from the deck
-//     theDealer.push(shuffledArray[i + 1])
+//     shuffledArray.shift([i]) // remove the card from the deck
+//     theDealer.unshift(shuffledArray[i + 1])
 //     console.log(`Dealer has: ${shuffledArray[i + 1]}`)
-//     shuffledArray.pop([i + 1])
+//     shuffledArray.shift([i + 1])
 // }
+console.log("🚀 ~ file: main.js ~ line 121 ~ shuffledArray", shuffledArray);
 
-playerCards.push(shuffledArray[0]) //pushes the first card to the player arr of cards
+function playerScore(){
+    playerTotalScore = playerCards.reduce(function (acc, cur) {//adds all numbers in the array together
+    return acc + cur;
+    });
+    
+}
+function dealerScore(){
+    dealerTotalScore = dealerCards.reduce(function (acc, cur) {
+    return acc + cur;
+    });
+}
 
+playerCards.unshift(shuffledArray[0]) //upshifts the first card to the front of the player arr of cards
 console.log(`Player has: ${shuffledArray[0]}`) //show the player their card
+playerScore()
+console.log("playerTotalScore1: " + playerTotalScore);
 
-shuffledArray.pop([0]) // remove the card from the deck
-console.log("🚀 ~ file: main.js ~ line 121 ~ shuffledArray", shuffledArray)
-
-dealerCards.push(shuffledArray[1])
+dealerCards.unshift(shuffledArray[1])
 console.log(`Dealer has: ${shuffledArray[1]}`)
-shuffledArray.pop([1])
+dealerScore()
+console.log("dealerTotalScore1: " + dealerTotalScore);
+//console.log("🚀 ~ file: main.js ~ line 121 ~ shuffledArray", shuffledArray);
 
-playerCards.push(shuffledArray[2])
+
+playerCards.unshift(shuffledArray[2])
 console.log(`Player has: ${shuffledArray[2]}`)
-shuffledArray.pop([2])
+playerScore()
+console.log("playerTotalScore2: " + playerTotalScore);
 
-dealerCards.push(shuffledArray[3])
+
+dealerCards.unshift(shuffledArray[3])
 console.log(`Dealer's whole card: ${shuffledArray[3]}`)
-shuffledArray.pop([3])
+dealerScore()
 
 
-
+shuffledArray.shift([0]);
+shuffledArray.shift([1]);
+shuffledArray.shift([2]);
+shuffledArray.shift([3]);
 console.log("Player: " + playerCards)
 console.log("Dealer: " + dealerCards)
 
+//console.log("🚀 ~ file: main.js ~ line 121 ~ shuffledArray", shuffledArray);
 
-let playerTotalScore = playerCards.reduce(function(acc, cur){ //adds all numbers in the array together
-    return acc + cur
-}) 
-let dealerTotalScore = dealerCards.reduce(function(acc, cur){
-    return acc + cur
-})
+
+
+
 console.log('playerTotalScore: ' + playerTotalScore)
 console.log('dealerTotalScore: ' + dealerTotalScore)
+console.log(shuffledArray.length)
+
+
+
+
+
+console.log("🚀 ~ file: main.js ~ line 121 ~ shuffledArray", shuffledArray);
+
+console.log("Player: " + playerCards);
+console.log("Dealer: " + dealerCards);
+
+console.log("playerTotalScore: " + playerTotalScore);
+console.log("dealerTotalScore: " + dealerTotalScore);
+
+
+hitButton.addEventListener("click", () => {
+  playerCards.push(shuffledArray[0]);
+  console.log("EventListener");
+  console.log("Player: " + playerCards);
+  playerScore()
+  
+  for(let i = 0; i < playerCards.length; i++){
+    console.log("inTheForLoop: " + playerCards[i])
+    if(playerCards[i] === 11 && playerTotalScore > 21){
+        playerTotalScore = playerTotalScore - 10
+    }
+    console.log(playerTotalScore)
+  }
+  
+  
+  
+  
+  
+  if (playerTotalScore > 21){
+
+  }
+  console.log("playerTotalScore: " + playerTotalScore);
+  shuffledArray.shift([0])
+  console.log("🚀 ~ file: main.js ~ line 121 ~ shuffledArray", shuffledArray);
+});
+
 
 // while(scoreKeeper < 22){ 
 
-// thePlayer.push(shuffledArray[5])
+// thePlayer.unshift(shuffledArray[5])
 // console.log(`<<Player has>>: ${shuffledArray[5]}`)
-// shuffledArray.pop([5])
+// shuffledArray.shift([5])
 // // }
 // playerTotalScore = thePlayer.reduce(function(acc, cur){ 
 //     return acc + cur
@@ -209,7 +289,7 @@ console.log('dealerTotalScore: ' + dealerTotalScore)
 
 
 //createUser("TRod")
-//createUser("Dage")
+//createUser("RamRod")
 
 //console.log(currentPlayers)
 
