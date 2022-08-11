dc4f3eba381e391eeae66c24e168f9d8382f2f76
Thilanka Rodrigo <65930513+Thilanka0824@users.noreply.github.com>
2022-08-11 14:10:26 -0700

    bug in ace logic

--- main.js
+++ main.js
@@ -1,130 +1,5 @@
+
 /*
-************************
-**   BlackJack Game   **
-************************
-
--create a user
-    -starts with $500
--create a function to add or subtract the n amount of money from the userMoney variable
-    -userMoney will start with $500
-    -money will added when the user wins, subtracted when they lose
--create a scoreKeeper variable to keep track of how many points each player has.
-    -this will determine if the user wins or loses the game 
-
-
--create a deck of cards with corresponding blackjack values
-    -cards 2,3,4,5,6,7,8,9 = equal their own numerical value
-    -cards 10, Jack, Queen, King = 10 //so i'll use 10, 10, 10, 10
-    -Ace is equal to 11 or 1
-        -will have to use a conditional to let the user determine if they want an 11 or 1
-    -total of 52 cards(13 of each suit)   
--load all the card values into an array that i'll randomize. not sure what to do with the ace(1 or 11)
-
--use the dealtCards arr to keep track each card that was dealt. 
-    -will be used to keep track of html id's of each card that was dealt
-
--user makes bet (starting value of $50)
-    -save bet amount in playerBet variable
-        -add/subtract that amount from playerWallet after the hand ends
-        -reset playerBet back to $50
-
-
--deal the starting cards from shuffledArray 
-    -1 to the player, 1 to the dealer, 1 to the player, 1 to the dealer(whole card. will not be visible to player)
--put dealt cards into respective players card array(holds cards to be added up for total score)
--remove dealt cards from shuffleArray (.shift)
-
-##########################################
-
-** game rules **
-
--if player has 21(a "natural"), and the dealer does not, he wins 1.5 times their bet
-
--if the dealer has 21, they collect the bets of every player, that does not have a natural
-
--if the dealer and a player BOTH have a natural, that players bet is returned
-
-##########################################
-
--conditional natural 21
-    
-    -player && dealer
-        if(playerTotalScore === 21 && dealerTotalScore === 21){
-        .unshift
-            add 0 to playerWallet
-        }
-
-    -player
-        -if(playerTotalScore === 21){
-            player wins!
-            add bet amount TIMES 1.5 to playerWallet
-        
-        }
-
-    -dealer
-        -if(dealerTotalScore === 21){
-            subtract bet amount from all players that don't have 21
-
-        }
-
--if no naturals are present
-  
-    while(gameOver = false ){          
-        
-        -player is asked if they want to "stand" or "hit"
-            -if (player === "stand"){
-                then continue 
-        }
-        else if (player === "hit"){
-            add first number in shuffleArray to thePlayerCards
-            show player their card
-            check if(playerCards) has an 11 in it
-                -ace adjuster
-
-        }
-    
-    }
-
-
- -if the player the player or dealer has an 11(Ace that could be a 1 or 11), it will need to dynamically change. 
-        -prompting the user would be too cumbersome 
-        -if playerTotalScore is greater than 10, all incoming aces are equal to 1
-        -if the player has an ace, and exceeds 21 on the next card, i'll subtract 10 from the ace
--i'll need to keep track of the ace issue in a separate function and only add to the total score once ace calculations are complete for both sides
-        -then add to the totalScore
-
-
--if the player chooses to stand AND player score is less than 21
-    -then move on to the dealers turn
-
--if dealer score is 17 or more, dealer stands.
-        -if player score is more than dealer score. player wins
-            -adds bet amount to player wallet
-            -will need a function to handle all the resets 
-        
-        -if player score is less than the dealer, dealer wins
-            -player bet is subtracted from player wallet
-            -bet resets
-
--if the dealer score is less than 17, dealer takes another card
-    -the dealer will continue to hit until their score is 17 or more
-        -ace logic will apply here
-    -if the dealer busts, then every player that is still in the game wins
-        -adds bet amount to payer wallet
-        -resets the everything for a new round.
-
--will use two diff resets.
-    -one to reset the round.
-        -reset bet amount
-        -clear the cards for the round
-    -one to reset the game
-        -after a a player loses all their money OR a player presses the New game button
-            -reset bet amount
-            -reset player wallet to $500
-            -reset all the cards being shown
-            -re-shuffle the shuffledArray
-            -reset Scorekeeper variables
-
 
 *********************
 *** stretch goals *** 
@@ -137,119 +12,114 @@
 
 */
 
-let playerCards = [] //holds the players cards //used by the score keeper
-let dealerCards = [] //holds the dealers cards //used by the score keeper
+let playerCards = []; //holds the players cards //used by the score keeper
+let dealerCards = []; //holds the dealers cards //used by the score keeper
 
-let playerWallet = 500
+let playerWallet = 500;
 
+let playerTotalScore = 0;
+let playerFinalScore = 0;
+let dealerTotalScore = 0;
+let dealerFinalScore = 0;
 
-let playerTotalScore = 0
-let playerFinalScore = 0
-let dealerTotalScore = 0
-
-const fullDeck = [2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11]
+const fullDeck = [
+  2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10,
+  10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8,
+  9, 10, 10, 10, 10, 11,   11,11,11,11,11,11,11,11,11,11
+];
 
 let shuffledArray = fullDeck.sort((a, b) => 0.5 - Math.random()); //randomizes the deck //Math.random returns a random number between 0 and 1
-let dealtCards = []
-let gameOver = false
-
-
+let dealtCards = [];
+let roundOver = false;
 
 //QuerySelectors
-let hitButton = document.querySelector('#hit-button')
-let standButton = document.querySelector('#stand-button')
-let dealCardsButton = document.querySelector('#deal-cards')
+let hitButton = document.querySelector("#hit-button");
+let standButton = document.querySelector("#stand-button");
+let dealCardsButton = document.querySelector("#deal-cards");
 
-let displayArea = document.querySelector('#display-area-div')
-let playerCardsDisplayArea = document.querySelector('#player-1')
-let dealerCardsDisplayArea = document.querySelector('#dealer')
+let displayArea = document.querySelector("#display-area-div");
+let playerCardsDisplayArea = document.querySelector("#player-1");
+let suitPic = document.querySelector('#suit-pic')
+let dealerCardsDisplayArea = document.querySelector("#dealer");
 
-
-// while(gameOver === false){
-// for(let i = 0; i < shuffledArray; i++){
-//     thePlayer.unshift(shuffledArray[i])
-//     console.log(`Player has: ${shuffledArray[i]}`) //show the player their card
-//     shuffledArray.shift([i]) // remove the card from the deck
-//     theDealer.unshift(shuffledArray[i + 1])
-//     console.log(`Dealer has: ${shuffledArray[i + 1]}`)
-//     shuffledArray.shift([i + 1])
-// }
 console.log("🚀 ~ file: main.js ~ line 121 ~ shuffledArray", shuffledArray);
-function nextRound(){
-    playerCards = []
-    dealerCards = []
-    displayArea.innerText = 'Dealing New Cards'
 
+
+function nextRound() {
+  if (roundOver === true) {
+    playerCards = [];
+    dealerCards = [];
+    playerTotalScore = 0;
+    dealerTotalScore = 0;
+   
+    displayArea.innerText = "New Game";
+    displayArea.innerText = "";
+  }
 }
 
-
-
-function playerScore(){
-    playerTotalScore = playerCards.reduce(function (acc, cur) {//adds all numbers in the array together
+function playerScore() {
+  playerTotalScore = playerCards.reduce(function (acc, cur) {
+    //adds all numbers in the array together
     return acc + cur;
-    });
-    
+  });
 }
-function dealerScore(){
-    dealerTotalScore = dealerCards.reduce(function (acc, cur) {
+
+function dealerScore() {
+  dealerTotalScore = dealerCards.reduce(function (acc, cur) {
     return acc + cur;
-    });
+  });
 }
 
-function dealInitialCards(){
-playerCards.unshift(shuffledArray[0]) //upshifts the first card to the front of the player arr of cards
-console.log(`Player has: ${shuffledArray[0]}`) //show the player their card
-playerScore()
-console.log("playerTotalScore1: " + playerTotalScore);
-playerCardsDisplayArea.innerText = "Player 1:   " + playerCards
+function dealInitialCards() {
+  
+  playerCards.unshift(shuffledArray[0]); //unshifts the first card to the front of the player arr of cards
+  console.log(`Player has: ${shuffledArray[0]}`); //show the player their card
+  playerScore();
+  playerFinalScore = playerTotalScore;
+  console.log("playerTotalScore1: " + playerTotalScore);
+  console.log("playerFinalScore1: " + playerFinalScore);
+  playerCardsDisplayArea.innerText = "Player 1:   " + playerCards;
 
+  dealerCards.unshift(shuffledArray[1]);
+  console.log(`Dealer has: ${shuffledArray[1]}`);
+  dealerScore();
+  console.log("dealerTotalScore1: " + dealerTotalScore);
+  //console.log("🚀 ~ file: main.js ~ line 121 ~ shuffledArray", shuffledArray);
+  dealerCardsDisplayArea.innerText = "Dealer:   " + dealerCards;
 
-dealerCards.unshift(shuffledArray[1])
-console.log(`Dealer has: ${shuffledArray[1]}`)
-dealerScore()
-console.log("dealerTotalScore1: " + dealerTotalScore);
-//console.log("🚀 ~ file: main.js ~ line 121 ~ shuffledArray", shuffledArray);
-dealerCardsDisplayArea.innerText = "Dealer:   " + dealerCards;
+  playerCards.unshift(shuffledArray[2]);
+  console.log(`Player has: ${shuffledArray[2]}`);
+  playerScore();
+  playerFinalScore = playerTotalScore;
+  console.log("playerTotalScore2: " + playerTotalScore);
+  console.log("playerFinalScore2: " + playerFinalScore);
+  playerCardsDisplayArea.innerText = "Player 1:   " + playerCards;
 
-playerCards.unshift(shuffledArray[2])
-console.log(`Player has: ${shuffledArray[2]}`)
-playerScore()
-console.log("playerTotalScore2: " + playerTotalScore);
-playerCardsDisplayArea.innerText = "Player 1:   " + playerCards;
+  dealerCards.unshift(shuffledArray[3]);
+  console.log(`Dealer's whole card: ${shuffledArray[3]}`);
+  dealerScore();
+  dealerCardsDisplayArea.innerText = "Dealer:   " + dealerCards;
 
-
-dealerCards.unshift(shuffledArray[3])
-console.log(`Dealer's whole card: ${shuffledArray[3]}`)
-dealerScore()
-dealerCardsDisplayArea.innerText = "Dealer:   " + dealerCards;
-
-shuffledArray.shift([0]);
-shuffledArray.shift([1]);
-shuffledArray.shift([2]);
-shuffledArray.shift([3]);
-console.log("Player: " + playerCards)
-console.log("Dealer: " + dealerCards)
+  shuffledArray.shift([0]);
+  shuffledArray.shift([1]);
+  shuffledArray.shift([2]);
+  shuffledArray.shift([3]);
+  console.log("Player: " + playerCards);
+  console.log("Dealer: " + dealerCards);
 }
 
-
-dealInitialCards()
+dealInitialCards();
 
 if (playerTotalScore === 21) {
   displayArea.innerText = "BLACKJACK!!!";
+  roundOver = true;
 }
 
 //console.log("🚀 ~ file: main.js ~ line 121 ~ shuffledArray", shuffledArray);
 
-
-
-
-console.log('playerTotalScore: ' + playerTotalScore)
-console.log('dealerTotalScore: ' + dealerTotalScore)
-console.log(shuffledArray.length)
-
-
-
-
+console.log("playerTotalScore: " + playerTotalScore);
+console.log("dealerTotalScore: " + dealerTotalScore);
+console.log(shuffledArray.length);
 
 console.log("🚀 ~ file: main.js ~ line 121 ~ shuffledArray", shuffledArray);
 
@@ -260,89 +130,95 @@ console.log("playerTotalScore: " + playerTotalScore);
 console.log("dealerTotalScore: " + dealerTotalScore);
 
 
+//BUTTONS
 hitButton.addEventListener("click", () => {
   playerCards.push(shuffledArray[0]);
+  shuffledArray.shift([0]);
   console.log("EventListener");
   console.log("Player: " + playerCards);
-  playerScore()
+  playerScore();
+  playerFinalScore = playerTotalScore;
   playerCardsDisplayArea.innerText = "Player 1:   " + playerCards;
-  for(let i = 0; i < playerCards.length; i++){
-    console.log("inTheForLoop: " + playerCards[i])
-    if(playerTotalScore === 21){
-        displayArea.innerText = "BLACKJACK!!!"
-    }
-    if(playerCards[i] === 11 && playerTotalScore > 21){
-        playerTotalScore = playerTotalScore - 10
-    }
-    console.log(playerTotalScore)
+  
+  for (let i = 0; i < playerCards.length; i++) {
     
-    if(playerTotalScore > 21){
-        console.log(displayArea.innerText = "BUST!")
-         
+    console.log("inTheForLoop: " + playerCards[i]);
+    
+    if (playerFinalScore === 21) {
+      displayArea.innerText = "BLACKJACK!!!";
+      roundOver = true
+      hitButton.removeEventListener()
+    }
+    if (playerCards[i] === 11 && playerTotalScore > 21) {
+      playerTotalScore = playerTotalScore - 10;
+      playerFinalScore = playerTotalScore
+      console.log("playerFinalScore: " + playerFinalScore)
+      if(playerFinalScore > 21){
+        console.log((displayArea.innerText = "BUST!"));
+        roundOver = true;
+      }
     }
 
-  }
-  
-  
-  
-  
-  
-  if (playerTotalScore > 21){
+    console.log(playerTotalScore);
 
+    if (playerFinalScore > 21) {
+      console.log((displayArea.innerText = "BUST!"));
+      roundOver = true;
+    }
+  }
+
+  if (playerTotalScore > 21) {
   }
   console.log("playerTotalScore: " + playerTotalScore);
-  shuffledArray.shift([0])
+  shuffledArray.shift([0]);
   console.log("🚀 ~ file: main.js ~ line 121 ~ shuffledArray", shuffledArray);
 });
 
-dealCardsButton.addEventListener('click', () => {
-    playerCards = []
-    dealerCards = []
-    playerTotalScore = 0
-    dealerTotalScore = 0
-    dealInitialCards()
-})
-// while(scoreKeeper < 22){ 
+standButton.addEventListener("click", () => {
+  displayArea.innerText = "Stand"
+  if (dealerTotalScore === 21){
 
-// thePlayer.unshift(shuffledArray[5])
-// console.log(`<<Player has>>: ${shuffledArray[5]}`)
-// shuffledArray.shift([5])
-// // }
-// playerTotalScore = thePlayer.reduce(function(acc, cur){ 
-//     return acc + cur
-// }) 
-// console.log("playerTotalScore: " + playerTotalScore)
+  }
 
+  if(dealerFinalScore < 16){
+  
+  dealerCards.push(shuffledArray[0])
+  shuffledArray.shift([0]);
+  
+  console.log("EventListener");
+  console.log("Dealer: " + dealerCards);
+  dealerScore();
+  dealerFinalScore = dealerTotalScore;
+  dealerCardsDisplayArea.innerText = "Dealer: " + dealerCards;
+  console.log("dealerFinalScore: " + dealerFinalScore);
+  }
+   for (let i = 0; i < dealerCards.length; i++) {
+     if (dealerFinalScore === 21 && dealerFinalScore === 21) {
+       displayArea.innerText = "PUSH";
+     }
+     if (dealerCards[i] === 11 && dealerTotalScore > 21) {
+       dealerTotalScore = dealerTotalScore - 10;
+       dealerFinalScore = dealerTotalScore;
+       console.log("dealerFinalScore: " + dealerFinalScore);
+       if (dealerFinalScore > 21) {
+         console.log((displayArea.innerText = "BUST!"));
+         roundOver = true;
+       }
+     }
+   }
+  
+ 
 
+});
 
+dealCardsButton.addEventListener("click", () => {
+  playerCards = [];
+  dealerCards = [];
+  playerTotalScore = 0;
+  dealerTotalScore = 0;
+  displayArea.innerText = "clear all";
+  nextRound()
+  setTimeout(dealInitialCards(), 3000);
+});
 
-// for(let i = 0; i < shuffledArray.length; i++){
-    
-
-// }
-
-
-
-
-
-// }   // <-- for thr while loop 
-
-
-
-//createUser("TRod")
-//createUser("RamRod")
-
-//console.log(currentPlayers)
-
-
-// for(let i = 0; i < fullDeck.length; i++){
-//     console.log(fullDeck[i])
-// }
-
-
-console.log(shuffledArray.length)
-
-
-
-
-
+console.log(shuffledArray.length);
--- scratch.js
+++ scratch.js
@@ -15,3 +15,135 @@
 //     currentPlayers.push(user)
 //     return user
 // }
+
+
+//////////////////////
+/*
+************************
+**   BlackJack Game   **
+************************
+
+-create a user
+    -starts with $500
+-create a function to add or subtract the n amount of money from the userMoney variable
+    -userMoney will start with $500
+    -money will added when the user wins, subtracted when they lose
+-create a scoreKeeper variable to keep track of how many points each player has.
+    -this will determine if the user wins or loses the game 
+
+
+-create a deck of cards with corresponding blackjack values
+    -cards 2,3,4,5,6,7,8,9 = equal their own numerical value
+    -cards 10, Jack, Queen, King = 10 //so i'll use 10, 10, 10, 10
+    -Ace is equal to 11 or 1
+        -will have to use a conditional to let the user determine if they want an 11 or 1
+    -total of 52 cards(13 of each suit)   
+-load all the card values into an array that i'll randomize. not sure what to do with the ace(1 or 11)
+
+-use the dealtCards arr to keep track each card that was dealt. 
+    -will be used to keep track of html id's of each card that was dealt
+
+-user makes bet (starting value of $50)
+    -save bet amount in playerBet variable
+        -add/subtract that amount from playerWallet after the hand ends
+        -reset playerBet back to $50
+
+
+-deal the starting cards from shuffledArray 
+    -1 to the player, 1 to the dealer, 1 to the player, 1 to the dealer(whole card. will not be visible to player)
+-put dealt cards into respective players card array(holds cards to be added up for total score)
+-remove dealt cards from shuffleArray (.shift)
+
+##########################################
+
+** game rules **
+
+-if player has 21(a "natural"), and the dealer does not, he wins 1.5 times their bet
+
+-if the dealer has 21, they collect the bets of every player, that does not have a natural
+
+-if the dealer and a player BOTH have a natural, that players bet is returned
+
+##########################################
+
+-conditional natural 21
+    
+    -player && dealer
+        if(playerTotalScore === 21 && dealerTotalScore === 21){
+        .unshift
+            add 0 to playerWallet
+        }
+
+    -player
+        -if(playerTotalScore === 21){
+            player wins!
+            add bet amount TIMES 1.5 to playerWallet
+        
+        }
+
+    -dealer
+        -if(dealerTotalScore === 21){
+            subtract bet amount from all players that don't have 21
+
+        }
+
+-if no naturals are present
+  
+    while(gameOver = false ){          
+        
+        -player is asked if they want to "stand" or "hit"
+            -if (player === "stand"){
+                then continue 
+        }
+        else if (player === "hit"){
+            add first number in shuffleArray to thePlayerCards
+            show player their card
+            check if(playerCards) has an 11 in it
+                -ace adjuster
+
+        }
+    
+    }
+
+
+ -if the player the player or dealer has an 11(Ace that could be a 1 or 11), it will need to dynamically change. 
+        -prompting the user would be too cumbersome 
+        -if playerTotalScore is greater than 10, all incoming aces are equal to 1
+        -if the player has an ace, and exceeds 21 on the next card, i'll subtract 10 from the ace
+-i'll need to keep track of the ace issue in a separate function and only add to the total score once ace calculations are complete for both sides
+        -then add to the totalScore
+
+
+-if the player chooses to stand AND player score is less than 21
+    -then move on to the dealers turn
+
+-if dealer score is 17 or more, dealer stands.
+        -if player score is more than dealer score. player wins
+            -adds bet amount to player wallet
+            -will need a function to handle all the resets 
+        
+        -if player score is less than the dealer, dealer wins
+            -player bet is subtracted from player wallet
+            -bet resets
+
+-if the dealer score is less than 17, dealer takes another card
+    -the dealer will continue to hit until their score is 17 or more
+        -ace logic will apply here
+    -if the dealer busts, then every player that is still in the game wins
+        -adds bet amount to payer wallet
+        -resets the everything for a new round.
+
+-will use two diff resets.
+    -one to reset the round.
+        -reset bet amount
+        -clear the cards for the round
+    -one to reset the game
+        -after a a player loses all their money OR a player presses the New game button
+            -reset bet amount
+            -reset player wallet to $500
+            -reset all the cards being shown
+            -re-shuffle the shuffledArray
+            -reset Scorekeeper variables
+
+
+*/
